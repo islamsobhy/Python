e0fe4c35ac51c82608c5dc33b676d6348a490898
islamsobhy <islamsobhy1991@gmail.com>
2025-08-12 10:27:57 +0300

    adding new files

---
+++ 6_conditions_ternary.py
@@ -0,0 +1,12 @@
+# age = 16
+# if age >= 18:
+#     print ("Can drive")
+# else:
+#     print("can't drive")
+
+#the above example is 5 lines code, we can reduce numbers of lines using the below way:
+
+
+age = 18
+message = "can drive" if age >=18 else "can't drive"
+print (message)
